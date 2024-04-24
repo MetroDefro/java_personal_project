ac4bf629e727f3a5560c9e5be95ddcf695b5706c
HEKim <khu4237@gmail.com>
2024-04-24 19:24:18 +0900

    Level1 완료: 완성 코드 불필요한 주석 제거

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -5,30 +5,20 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-//        // 5. 연산 결과 10개를 저장할 수 있는 배열을 선언 및 생성하고 연산의 결과를 저장합니다.
-//        int[] results = new int[10]; // 연산의 결과를 저장할 수 있도록 적합한 타입의 배열을 생성합니다.
-//        int index = 0; // 연산의 결과가 저장된 배열의 마지막 index를 저장하는 변수를 선언
-
-        // 7. 연산 결과가 10개로 고정되지 않고 무한이 저장될 수 있도록 소스 코드를 수정합니다.
         LinkedList<Integer> results = new LinkedList<>(); // 연산 결과를 저장하기 적합한 컬렉션인 LinkedList 생성
 
-        // 1. Scanner를 사용하여 양의 정수 2개(0 포함)를 전달 받을 수 있습니다.
         Scanner sc = new Scanner(System.in); // 스캐너 생성
 
-        // 4. 반복문을 사용하여 반복의 종료를 알려주는 “exit” 문자열을 입력하기 전까지 무한으로 계산을 진행할 수 있도록 소스 코드를 수정합니다.
         boolean isEnd = false; // 종료 할지 여부를 boolean에 저장하기 위해 변수 생성.
         do { // 첫번째는 무조건 실행.
-            // 1. Scanner를 사용하여 양의 정수 2개(0 포함)를 전달 받을 수 있습니다.
             System.out.print("첫 번째 숫자를 입력하세요: ");
             int firstNumber = sc.nextInt(); // int를 입력 받는다.
             System.out.print("두 번째 숫자를 입력하세요: ");
             int secondNumber = sc.nextInt(); // int를 입력 받는다.
 
-            // 2. Scanner를 사용하여 사칙연산 기호를 전달 받을 수 있습니다.
             System.out.print("사칙연산 기호를 입력하세요: ");
             char operator = sc.next().charAt(0); // 입력 받은 string의 인덱스 0번 char을 가져온다.
 
-            // 3. 입력받은 양의 정수 2개와 사칙연산 기호를 사용하여 연산을 진행한 후 결과값을 출력합니다.
             int result = 0;
             switch (operator) { // operator 값에 따라 네 가지 case로 분류
                 case '+': // 덧셈 연산 수행하여 result에 저장
@@ -50,18 +40,6 @@ public class App {
 
             System.out.println("결과: " + result);
 
-//            // 6. 연산 결과가 10개를 초과하는 경우 가장 먼저 저장된 결과를 삭제하고 새로운 연산 결과가 저장될 수 있도록 소스 코드를 수정합니다.
-//            if(index >= 9) { // 인덱스가 9를 넘을 때만
-//                for(int i = 0; i < results.length - 1; i++) { // 인덱스 0부터 8까지 순회하며
-//                    results[i] = results[i + 1]; // 다음 인덱스의 값을 현재 인덱스 자리에 넣는다
-//                }
-//                results[index] = result; // 인덱스 9에 결과를 저장
-//            } else { // 인덱스 9 미만의 경우
-//                // 5. 연산 결과 10개를 저장할 수 있는 배열을 선언 및 생성하고 연산의 결과를 저장합니다.
-//                results[index++] = result; // result를 저장하고 index 증가
-//            }
-
-            // 7. 연산 결과가 10개로 고정되지 않고 무한이 저장될 수 있도록 소스 코드를 수정합니다.
             results.add(result); // 리스트에 결과 추가
 
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
@@ -69,7 +47,6 @@ public class App {
                 results.removeFirst(); // 첫 결과 삭제
             }
 
-            // 8. “inquiry”라는 문자열이 입력되면 저장된 연산 결과 전부를 출력합니다.
             System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
             if(sc.next().equals("inquiry")) { // 입력 받은 답변이 "inquiry"일 경우
                 for(int element : results ) { // foreach 문 사용하여 results linkedlist 순회
