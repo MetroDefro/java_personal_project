50f12d4e705592833f237904ac9d57f2de4546c4
HEKim <khu4237@gmail.com>
2024-04-25 14:48:28 +0900

    Level2-2. Level 1에서 구현한 App 클래스의 main 메서드에 Calculator 클래스가 활용될 수 있도록 수정합니다.

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,11 +1,11 @@
 package calculator;
 
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        LinkedList<Integer> results = new LinkedList<>(); // 연산 결과를 저장하기 적합한 컬렉션인 LinkedList 생성
+        // 2. Level 1에서 구현한 App 클래스의 main 메서드에 Calculator 클래스가 활용될 수 있도록 수정합니다.
+        Calculator calculator = new Calculator();
 
         Scanner sc = new Scanner(System.in); // 스캐너 생성
 
@@ -19,44 +19,32 @@ public class App {
             System.out.print("사칙연산 기호를 입력하세요: ");
             char operator = sc.next().charAt(0); // 입력 받은 string의 인덱스 0번 char을 가져온다.
 
-            int result = 0;
-            switch (operator) { // operator 값에 따라 네 가지 case로 분류
-                case '+': // 덧셈 연산 수행하여 result에 저장
-                    result = firstNumber + secondNumber;
-                    break;
-                case '-': // 뺄셈 연산 수행하여 result에 저장
-                    result = firstNumber - secondNumber;
-                    break;
-                case '*': // 곱샘 연산 수행하여 result에 저장
-                    result = firstNumber * secondNumber;
-                    break;
-                case '/': // 나눗셈 연산 수행하여 result에 저장
-                    if(secondNumber == 0) // 분모가 0일 경우 아래 문구 출력
-                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                    else
-                        result = firstNumber / secondNumber;
-                    break;
-            }
+            try {
+                int result = calculator.calculate(firstNumber, secondNumber, operator);
 
-            System.out.println("결과: " + result);
+                System.out.println("결과: " + result);
 
-            results.add(result); // 리스트에 결과 추가
+                calculator.results.add(result); // 리스트에 결과 추가
 
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            if(sc.next().equals("remove")) { // 입력 받은 답변이 "remove"일 경우
-                results.removeFirst(); // 첫 결과 삭제
-            }
-
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            if(sc.next().equals("inquiry")) { // 입력 받은 답변이 "inquiry"일 경우
-                for(int element : results ) { // foreach 문 사용하여 results linkedlist 순회
-                    System.out.print(element + " "); // 출력하고 한 칸 띄기
+                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                if(sc.next().equals("remove")) { // 입력 받은 답변이 "remove"일 경우
+                    calculator.results.removeFirst(); // 첫 결과 삭제
                 }
-                System.out.println(); // 구분을 위한 한 줄 내리기
+
+                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                if(sc.next().equals("inquiry")) { // 입력 받은 답변이 "inquiry"일 경우
+                    for(int element : calculator.results ) { // foreach 문 사용하여 results linkedlist 순회
+                        System.out.print(element + " "); // 출력하고 한 칸 띄기
+                    }
+                    System.out.println(); // 구분을 위한 한 줄 내리기
+                }
+
+                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+                isEnd = sc.next().equals("exit"); // 입력 받은 답변이 "exit"라면 isEnd는 true 아니면 false 유지
+            } catch (InputErrorException e) {
+                System.out.println(e.getMessage());
             }
 
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            isEnd = sc.next().equals("exit"); // 입력 받은 답변이 "exit"라면 isEnd는 true 아니면 false 유지
         } while (!isEnd);
     }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,7 +5,8 @@ import java.util.LinkedList;
 public class Calculator {
     // 1. 양의 정수 2개(0 포함)와 연산 기호를 매개변수로 받아 사칙연산(+,-,*,/)
     // 기능을 수행한 후 결과 값을 반환하는 메서드와 연산 결과를 저장하는 컬렉션 타입 필드를 가진 Calculator 클래스를 생성합니다.
-    private LinkedList<Integer> results = new LinkedList<>(); // 연산 결과를 저장하기 적합한 컬렉션인 LinkedList 선언 및 생성
+    // 2번 기능을 수행하기 위해 잠시 public으로 바꿨습니다.
+    public LinkedList<Integer> results = new LinkedList<>(); // 연산 결과를 저장하기 적합한 컬렉션인 LinkedList 선언 및 생성
 
     public int calculate(int firstNumber, int secondNumber, char operator) throws InputErrorException {
         int result = 0;
