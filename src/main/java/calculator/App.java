6b9bf1a92469b6d22214dece6d461780a0c2b7d7
HEKim <khu4237@gmail.com>
2024-04-25 15:40:56 +0900

    Level2-7. Calculator 클래스에 반지름을 매개변수로 전달받아 원의 넓이를 계산하여 반환해주는 메서드를 구현합니다.

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -11,42 +11,62 @@ public class App {
 
         boolean isEnd = false; // 종료 할지 여부를 boolean에 저장하기 위해 변수 생성.
         do { // 첫번째는 무조건 실행.
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int firstNumber = sc.nextInt(); // int를 입력 받는다.
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int secondNumber = sc.nextInt(); // int를 입력 받는다.
+            // 7. Calculator 클래스에 반지름을 매개변수로 전달받아 원의 넓이를 계산하여 반환해주는 메서드를 구현합니다.
+            System.out.println("계산기 옵션(번호 입력): 1. 사칙연산, 2. 원의 너비 구하기"); // 사칙연산 원의 너비 중 선택하도록 제시
+            switch (sc.nextInt()) { // 입력 받은 값에 따라 두 갈래로 나뉜다.
+                case 1: // 사칙 연산 로직
+                    System.out.print("첫 번째 숫자를 입력하세요: ");
+                    int firstNumber = sc.nextInt(); // int를 입력 받는다.
+                    System.out.print("두 번째 숫자를 입력하세요: ");
+                    int secondNumber = sc.nextInt(); // int를 입력 받는다.
 
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            char operator = sc.next().charAt(0); // 입력 받은 string의 인덱스 0번 char을 가져온다.
+                    System.out.print("사칙연산 기호를 입력하세요: ");
+                    char operator = sc.next().charAt(0); // 입력 받은 string의 인덱스 0번 char을 가져온다.
 
-            try {
-                int result = calculator.calculate(firstNumber, secondNumber, operator);
+                    try {
+                        int result = calculator.calculate(firstNumber, secondNumber, operator);
 
-                System.out.println("결과: " + result);
+                        System.out.println("결과: " + result);
 
-                // 3. App 클래스의 main 메서드에서 Calculator 클래스의 연산 결과를 저장하고 있는 컬렉션 필드에 직접 접근하지 못하도록 수정합니다. (캡슐화)
-                calculator.getResults().add(result); // 리스트에 결과 추가
+                        // 3. App 클래스의 main 메서드에서 Calculator 클래스의 연산 결과를 저장하고 있는 컬렉션 필드에 직접 접근하지 못하도록 수정합니다. (캡슐화)
+                        calculator.getResults().add(result); // 리스트에 결과 추가
 
-                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-                // 4. Calculator 클래스에 저장된 연산 결과들 중
-                // 가장 먼저 저장된 데이터를 삭제하는 기능을 가진 메서드를 구현한 후 App 클래스의 main 메서드에 삭제 메서드가 활용될 수 있도록 수정합니다.
-                if(sc.next().equals("remove")) { // 입력 받은 답변이 "remove"일 경우
-                    calculator.removeResult(); // 첫 결과 삭제
-                }
+                        System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                        // 4. Calculator 클래스에 저장된 연산 결과들 중
+                        // 가장 먼저 저장된 데이터를 삭제하는 기능을 가진 메서드를 구현한 후 App 클래스의 main 메서드에 삭제 메서드가 활용될 수 있도록 수정합니다.
+                        if(sc.next().equals("remove")) { // 입력 받은 답변이 "remove"일 경우
+                            calculator.removeResult(); // 첫 결과 삭제
+                        }
 
-                // 5. Calculator 클래스에 저장된 연산 결과들을 조회하는 기능을 가진 메서드를 구현한 후
-                // App 클래스의 main 메서드에 조회 메서드가 활용될 수 있도록 수정합니다.
-                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-                if(sc.next().equals("inquiry")) { // 입력 받은 답변이 "inquiry"일 경우
-                    calculator.inquiryResults(); // 결과 조회
-                }
+                        // 5. Calculator 클래스에 저장된 연산 결과들을 조회하는 기능을 가진 메서드를 구현한 후
+                        // App 클래스의 main 메서드에 조회 메서드가 활용될 수 있도록 수정합니다.
+                        System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                        if(sc.next().equals("inquiry")) { // 입력 받은 답변이 "inquiry"일 경우
+                            calculator.inquiryResults(); // 결과 조회
+                        }
 
-                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-                isEnd = sc.next().equals("exit"); // 입력 받은 답변이 "exit"라면 isEnd는 true 아니면 false 유지
-            } catch (InputErrorException e) {
-                System.out.println(e.getMessage());
+                        System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+                        isEnd = sc.next().equals("exit"); // 입력 받은 답변이 "exit"라면 isEnd는 true 아니면 false 유지
+
+                    } catch (InputErrorException e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                // 7. Calculator 클래스에 반지름을 매개변수로 전달받아 원의 넓이를 계산하여 반환해주는 메서드를 구현합니다.
+                case 2: // 원의 너비 구하기 로직
+                    System.out.print("원의 반지름을 입력하세요: ");
+                    int radius = sc.nextInt(); // int를 입력 받는다.
+
+                    double result = calculator.calculateCircleArea(radius); // 원 넓이 구하는 메서드 호출하고 값 result에 저장
+
+                    System.out.println("결과: " + result);
+
+                    calculator.getCircleAreaResult().add(result); // 컬렉션에 결과 저장
+                    calculator.inquiryCircleAreaResult(); // 원의 넓기 결과들 바로 전체 조회
+                    System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+                    isEnd = sc.next().equals("exit"); // 입력 받은 답변이 "exit"라면 isEnd는 true 아니면 false 유지
+                    break;
             }
-
         } while (!isEnd);
     }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -8,17 +8,30 @@ public class Calculator {
     // 3. App 클래스의 main 메서드에서 Calculator 클래스의 연산 결과를 저장하고 있는 컬렉션 필드에 직접 접근하지 못하도록 수정합니다. (캡슐화)
     private LinkedList<Integer> results; // 연산 결과를 저장하기 적합한 컬렉션인 LinkedList private로 선언
 
-    public LinkedList<Integer> getResults() { // results getter
-        return results;
-    };
+    // 7. Calculator 클래스에 반지름을 매개변수로 전달받아 원의 넓이를 계산하여 반환해주는 메서드를 구현합니다.
+    private static final double PI = 3.14; // 절대 변할 일 없는 필드이기 때문에 static final 활용
+    private LinkedList<Double> circleAreaResult; // 원의 넓이 결과를 저장하는 컬렉션 타입의 필드 선언
 
     // 6. Calculator 인스턴스를 생성(new)할 때 생성자를 통해 연산 결과를 저장하고 있는 컬렉션 필드가 초기화 되도록 수정합니다
     public Calculator() {
         results = new LinkedList<>(); // 생성자에서 컬렉션 필드를 초기화
+        circleAreaResult = new LinkedList<>(); // 생성자에서 컬렉션 필드를 초기화
     }
+
+    public LinkedList<Integer> getResults() { // results getter
+        return results;
+    };
 
     public void setResults(LinkedList<Integer> results) { // results setter
         this.results = results;
+    }
+
+    public LinkedList<Double> getCircleAreaResult() { // circleAreaResult getter
+        return circleAreaResult;
+    }
+
+    public void setCircleAreaResult(LinkedList<Double> circleAreaResult) { // circleAreaResult setter
+        this.circleAreaResult = circleAreaResult;
     }
 
     public int calculate(int firstNumber, int secondNumber, char operator) throws InputErrorException {
@@ -54,6 +67,19 @@ public class Calculator {
     // App 클래스의 main 메서드에 조회 메서드가 활용될 수 있도록 수정합니다.
     public void inquiryResults() {
         for(int element : results ) { // foreach 문 사용하여 results linkedlist 순회
+            System.out.print(element + " "); // 출력하고 한 칸 띄기
+        }
+        System.out.println(); // 구분을 위한 한 줄 내리기
+    }
+
+    // 7. Calculator 클래스에 반지름을 매개변수로 전달받아 원의 넓이를 계산하여 반환해주는 메서드를 구현합니다.
+    public double calculateCircleArea(int radius) { // 원의 넓이를 구하는 메서드
+        return PI * radius * radius; // 원 넓이 구하는 공식
+    }
+
+    // 원 넓이 컬렉션 조회
+    public void inquiryCircleAreaResult() {
+        for(double element : circleAreaResult ) { // foreach 문 사용하여 circleAreaResult linkedlist 순회
             System.out.print(element + " "); // 출력하고 한 칸 띄기
         }
         System.out.println(); // 구분을 위한 한 줄 내리기
