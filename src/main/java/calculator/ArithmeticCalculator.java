a9015a4f944ffc532cc22073004957b5b17b2d9a
HEKim <khu4237@gmail.com>
2024-05-02 11:54:45 +0900

    개인 과제 개선 - new 대신 OperatorFactory를 따로 만들어서 미리 만든 operator를 가져온다.

--- src/main/java/calculator/ArithmeticCalculator.java
+++ src/main/java/calculator/ArithmeticCalculator.java
@@ -4,22 +4,16 @@ import java.util.List;
 
 public class ArithmeticCalculator<T extends Number> extends Calculator {
 
-    // 계산을 진행할 메서드
-    public T calculate(T firstNumber, T secondNumber, char operator) {
-        // operatorFactory 메서드에서 받은 operator로 operator 호출하여 계산한다.
-        return operatorFactory(operator).operate(firstNumber, secondNumber);
+    private final OperatorFactory<T> operatorFactory;
+
+    public ArithmeticCalculator() {
+        this.operatorFactory = new OperatorFactory<>();
     }
 
-    // operator 기호로부터 해당하는 Operator를 리턴받기 위한 메서드
-    private Operator<T> operatorFactory(char operator) {
-        // 향상된 switch!
-        return switch (OperatorType.fromOperator(operator)) { // char 기호를 OperatorType으로 변환
-            case ADD -> new AddOperator(); // OperatorType에 따라 해당하는 Operator 리턴
-            case SUB -> new SubstractOperator();
-            case MUL -> new MultiplyOperator();
-            case DIV -> new DivideOperator();
-            case MOD -> new ModOperator();
-        };
+    // 계산을 진행할 메서드
+    public T calculate(T firstNumber, T secondNumber, char operator) {
+        // operatorFactory 객체로부터 operator 타입에 맞는 operator를 받아 operate 호출하여 계산한다.
+        return operatorFactory.GetOperator(OperatorType.fromOperator(operator)).operate(firstNumber, secondNumber);
     }
 
     public void inquiryBigger(double number) {
---
+++ src/main/java/calculator/OperatorFactory.java
@@ -0,0 +1,29 @@
+package calculator;
+
+// 오퍼레이터들을 생성하는 클래스
+public class OperatorFactory <T extends Number> {
+    private final AddOperator<T> addOperator; // 오퍼레이터들 변할 일 없으므로 final로 선언
+    private final SubstractOperator<T> substractOperator;
+    private final MultiplyOperator<T> multiplyOperator;
+    private final DivideOperator<T> divideOperator;
+    private final ModOperator<T> modOperator;
+
+    // 생성자에서 오퍼레이터 생성
+    public OperatorFactory() {
+        addOperator = new AddOperator<>();
+        substractOperator = new SubstractOperator<>();
+        multiplyOperator = new MultiplyOperator<>();
+        divideOperator = new DivideOperator<>();
+        modOperator = new ModOperator<>();
+    }
+
+    public Operator<T> GetOperator(OperatorType type) {
+        return switch (type) { // OperatorType에 따라 해당하는 Operator 리턴
+            case ADD -> addOperator;
+            case SUB -> substractOperator;
+            case MUL -> multiplyOperator;
+            case DIV -> divideOperator;
+            case MOD -> modOperator;
+        };
+    }
+}
